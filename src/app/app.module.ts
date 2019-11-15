781494776dba28ee907ccabfe4f96a18d8c3fbfd
Tomas <tomas.vissio1990@gmail.com>
2019-11-15 18:30:51 +0100

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from "@angular/core";
 import { PagesModule } from "./pages/pages.module";
 import { AppRoutingModule } from "./app-routing.module";
 
+//Components
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./login/register.component";
